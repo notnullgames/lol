d9d00aad95d111057c514efdd708ca2d250e12e6
David Konsumer <konsumer@jetboystudio.com>
2021-02-15 04:11:27 -0800

    prototyped tiles a bit, ysort still wonky

--- maps/Demo.gd
+++ maps/Demo.gd
@@ -8,4 +8,5 @@ export(String) var dialog_name
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.show_dialog(dialog_name, text, "left")
+	# Global.show_dialog(dialog_name, text, "left")
+	pass
