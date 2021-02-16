369c1a25f4f58c7268efd9f3e0e1ecd0d700e315
David Konsumer <konsumer@jetboystudio.com>
2021-02-16 14:32:20 -0800

    basic player animation

--- objects/Player.gd
+++ objects/Player.gd
@@ -5,6 +5,7 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+onready var animationState = $AnimationTree.get("parameters/playback")
 var velocity =  Vector2.ZERO
 
 func _physics_process(delta):
@@ -14,8 +15,12 @@ func _physics_process(delta):
 		input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 		input_vector = input_vector.normalized()
 		if input_vector != Vector2.ZERO:
+			$AnimationTree.set("parameters/Idle/blend_position", input_vector)
+			$AnimationTree.set("parameters/Walk/blend_position", input_vector)
+			animationState.travel("Walk")
 			velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		else:
+			animationState.travel("Idle")
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		velocity = move_and_slide(velocity)
 	
