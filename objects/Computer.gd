f7185f185c032abeec8d323779016c8fa53e87ef
David Konsumer <konsumer@jetboystudio.com>
2021-02-16 19:47:48 -0800

    broke computer

--- objects/Computer.gd
+++ objects/Computer.gd
@@ -8,10 +8,12 @@ var touching = false
 func _on_Computer_body_entered(body):
 	if body.name == "Player":
 		touching = true
+		$AnimationPlayer.play("On")
 
 func _on_Computer_body_exited(body):
 	if body.name == "Player":
 		touching = false
+		$AnimationPlayer.play("Off")
 
 func _input(event):
 	if event.is_action_pressed("ui_accept") and touching and Global.player_can_move:
