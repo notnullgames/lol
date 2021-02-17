757600f6a921508b274ef2cf7fc8a15edd30b943
David Konsumer <konsumer@jetboystudio.com>
2021-02-16 19:26:26 -0800

    basic frame of computer

--- objects/Computer.gd
+++ objects/Computer.gd
@@ -15,5 +15,4 @@ func _on_Computer_body_exited(body):
 
 func _input(event):
 	if event.is_action_pressed("ui_accept") and touching and Global.player_can_move:
-		print("dialog goes here, but it's broke.")
 		var dialog = Global.show_dialog("PX9000", "Hello and welcome to the computer.", "left")
